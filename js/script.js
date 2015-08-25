91efc8ce3fc9cfbf784589381cc195539e850348
Abin Thaha <abin@qburst.com>
2015-08-25 15:28:04 +0530

    Menu tab completed

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function show_hidden_menu() {
+  if(document.getElementById('hidden_menu').style.display == "none")
+  {
+    document.getElementById('hidden_menu').style.display = "block";
+    document.getElementById('menu_button').innerHTML = "X";
+  }
+  else {
+    document.getElementById('hidden_menu').style.display = "none";
+    document.getElementById('menu_button').innerHTML = "MENU";
+  }
+}
+
+function show_search() {
+  if(document.getElementById('search_div').style.display == "none")
+  {
+    document.getElementById('search_div').style.display = "block";
+  }
+  else {
+    document.getElementById('search_div').style.display = "none";
+  }
+}
