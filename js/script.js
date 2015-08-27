481d3abb45ed3d616b7c7413e49bafa4ac56163f
Abin Thaha <abin@qburst.com>
2015-08-27 17:05:09 +0530

    80% completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 function show_hidden_menu() {
   if(document.getElementById('hidden_menu').style.display == "none")
   {
+    if(document.getElementById('search_div').style.display == "block")
+    {
+      document.getElementById('search_div').style.display = "none";
+    }
     document.getElementById('hidden_menu').style.display = "block";
     document.getElementById('menu_button').innerHTML = "X";
   }
@@ -13,6 +17,10 @@ function show_hidden_menu() {
 function show_search() {
   if(document.getElementById('search_div').style.display == "none")
   {
+    if(document.getElementById('hidden_menu').style.display == "block")
+    {
+      document.getElementById('hidden_menu').style.display = "none";
+    }
     document.getElementById('search_div').style.display = "block";
   }
   else {
